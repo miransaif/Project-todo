a75fdb69c64f6299ab191c67ec2b52dd1da510aa
miransaif <miransaif16@gmail.com>
2022-07-03 19:09:42 +0530

    first commit

---
+++ src/components/Cards.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import EditTask from '../Actions/EditTask'
+
+const Cards = ({taskObj, index, deleteTask, updateListArray}) => {
+    const [modal, setModal] = useState(false);
+
+    const colors = [
+        {
+            primaryColor : "#FF6666",
+            secondaryColor : "#ECF3FC"
+        },
+        {
+            primaryColor : "#6AC47E",
+            secondaryColor : "#FEFAF1"
+        },
+        {
+            primaryColor : "#51E1ED",
+            secondaryColor : "#F2FAF1"
+        },
+        {
+            primaryColor : "#5DA3FA",
+            secondaryColor : "#FDF1F1"
+        },
+        {
+            primaryColor : "#B964F7",
+            secondaryColor : "#F3F0FD"
+        }
+    ]
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const updateTask = (obj) => {
+        updateListArray(obj, index)
+    }
+
+    const handleDelete = () => {
+        deleteTask(index)
+    }
+
+    return (
+        <div className = "card-wrapper mr-5">
+            <div className = "card-top" style={{"background-color": colors[index%5].primaryColor}}></div>
+            <div className = "task-holder">
+                <span className = "card-header" style={{"background-color": colors[index%5].secondaryColor, "border-radius": "10px"}}>{taskObj.Name}</span>
+                <p className = "mt-3">{taskObj.Description}</p>
+
+                <div style={{"position": "absolute", "right" : "20px", "bottom" : "20px"}}>
+                    <i className = "far fa-edit mr-5" style={{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {() => setModal(true)}></i>
+                    <i className="fas fa-trash-alt" style = {{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}></i>
+                </div>
+        </div>
+        <EditTask modal = {modal} toggle = {toggle} updateTask = {updateTask} taskObj = {taskObj}/>
+        </div>
+    );
+};
+
+export default Cards;
